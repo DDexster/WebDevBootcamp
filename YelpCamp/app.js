5951390f468201fcdbea7ef5569084986146806c
Dmitry Bondarenko <d.bondrnko@gmail.com>
2017-07-31 15:25:15 +0300

    Added Landing and Campgrounds views

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -8,7 +8,7 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
-appp.get("/campgrounds", function(req, res) {
+app.get("/campgrounds", function(req, res) {
     var campgrounds = [
         { name: "Salmon Greek", image: "https://farm4.staticflickr.com/3844/15335755172_33dec7e209.jpg" },
         { name: "Granite Hill", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg" },
