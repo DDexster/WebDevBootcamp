af75b3dff7b463d32edfc5a22622ec6b7584dce2
Dmitry Bondarenko <d.bondrnko@gmail.com>
2017-08-16 11:53:32 +0300

    AuthDemo done, using passport.js

--- Authenication/AuthDemo/app.js
+++ Authenication/AuthDemo/app.js
@@ -4,7 +4,7 @@ var express = require('express'),
     passport = require('passport'),
     User = require('./models/user'),
     bodyParser = require('body-parser'),
-    localStrategy = require('passport-local'),
+    LocalStrategy = require('passport-local'),
     passportLocalMongoose = require('passport-local-mongoose');
 
 //=======================APP Settings===================
@@ -20,15 +20,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost/auth_demo');
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+//===================FUNCTIONS===========================
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/login');
+    }
+}
 
 //===================ROUTES===========================
 app.get('/', function(req, res) {
     res.render('home');
 });
 
-app.get('/secret', function(req, res) {
+app.get('/secret', isLoggedIn, function(req, res) {
     res.render('secret');
 });
 
@@ -37,7 +47,7 @@ app.get('/register', function(req, res) {
 });
 
 app.post('/register', function(req, res) {
-    User.register(new User({ username: req.body.user.username }), req.body.user.password, function(err, user) {
+    User.register(new User({ username: req.body.username }), req.body.password, function(err, user) {
         if (err) {
             console.log("Error");
             res.render("register");
@@ -49,7 +59,21 @@ app.post('/register', function(req, res) {
     });
 });
 
+app.get("/login", function(req, res) {
+    res.render("login");
+});
 
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res) {
+
+});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
 
 // =============Listen for CLOUD9=====================
 // app.listen(process.env.PORT, process.env.IP, function(){
